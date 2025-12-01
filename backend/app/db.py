0eb1c71e6e71c43072c458d5709cfb902852b26a
Juels0nB <mambini.tech@gmail.com>
2025-12-01 19:40:40 +0000

    Update the design of profile and admin pages , added logic in cart and adjust the back to the cloud and deal whith images using cloudinari

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,6 @@
 import os
 from mongoengine import connect
-MONGO_URL = os.getenv("MONGODB_URI","mongodb://localhost:27017/mydatabase")
+
+MONGO_URL = os.getenv("MONGODB_URI", "mongodb://localhost:27017/mydatabase")
+
 connect(host=MONGO_URL)
