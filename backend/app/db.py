44021f11600022d2339f83464b26e77bafe90c29
Juels0nB <mambini.tech@gmail.com>
2025-12-01 18:50:31 +0000

    Update the design of profile and admin pages , added logic in cart and adjust the back to the cloud and deal whith images using cloudinari

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,4 +1,4 @@
 import os
 from mongoengine import connect
-MONGO_URL = os.getenv("mongodb+srv://juelsonbalanga_db_user:JyKDNzicoflgVfzA@mambinistore.mxfpxrp.mongodb.net/?appName=MambiniStore","mongodb://localhost:27017/mydatabase")
+MONGO_URL = os.getenv("mongodb+srv://Vercel-Admin-Mambini-Store:CtpFvdifk3wJa4EX@mambini-store.ywfkrvf.mongodb.net/?retryWrites=true&w=majority","mongodb://localhost:27017/mydatabase")
 connect(host=MONGO_URL)
